ccd6640d65c4b1104fff79c037983e09323eb55d
Vishnu Kasilya P <vishnukp21064@gmail.com>
2024-07-17 21:27:05 +0530

    SQ

--- Stack&Queue/Easy/queue_using_stack.java
+++ Stack&Queue/Easy/queue_using_stack.java
@@ -1,7 +1,41 @@
 import java.util.Stack;
 // Problem link: https://leetcode.com/problems/implement-queue-using-stacks/
+// Using 2 stacks: tc: O(n) sc: O(2n)
+// class MyQueue {
+//     Stack<Integer> s1=new Stack<>();
+//     Stack<Integer> s2=new Stack<>();
+//     public MyQueue() {
+        
+//     }
+    
+//     public void push(int x) {
+//         while(!s1.isEmpty()){
+//             s2.add(s1.peek());
+//             s1.pop();
+//         }
+//         s1.add(x);
+//         while(!s2.isEmpty()){
+//             s1.add(s2.peek());
+//             s2.pop();
+//         }
+
+//     }
+    
+//     public int pop() {
+//         return s1.pop();
+//     }
+    
+//     public int peek() {
+//         return s1.peek();
+//     }
+    
+//     public boolean empty() {
+//         return (s1.size()==0);
+//     }
+// }
+
 class MyQueue {
-    // Optimal - tc: O(1) sc: O(2n)
+    // Optimal - tc: O(1) amortized sc: O(2n)
     public MyQueue() {
     }
     Stack<Integer> input=new Stack<>();
--- Stack&Queue/Easy/stack_using_queue.java
+++ Stack&Queue/Easy/stack_using_queue.java
@@ -2,11 +2,22 @@ import java.util.*;
 // Problem link: https://leetcode.com/problems/implement-stack-using-queues/
 class MyStack {
     // tc - O(n) sc: O(n)
+    // Queue<Integer> q1=new LinkedList<>();
+    // Queue<Integer> q2=new LinkedList<>();
     public MyStack() {
     }
     Queue<Integer> q=new LinkedList<>();
     
     public void push(int x) {
+        // 2 queues - tc: O(n) sc: O(2n)
+        // q2.add(x);
+        // while(!q1.isEmpty()){
+        //   q2.add(q1.peek());
+        //   q1.poll();
+        // } 
+        // Queue temp=q2;
+        // q2=q1;
+        // q1=temp;
         q.add(x);
         for(int i=0;i<q.size()-1;i++){
             q.add(q.peek());
