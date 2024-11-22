2630a01d8104344d91e1f87e28f2b007ae35b30f
Vishnu Kasilya <125987277+VishnuAmit@users.noreply.github.com>
2024-11-22 09:12:16 +0530

    iterative method

--- BinaryTree/Medium/rightview.java
+++ BinaryTree/Medium/rightview.java
@@ -1,20 +1,50 @@
 // Problem link: https://leetcode.com/problems/binary-tree-right-side-view/
 class Solution {
-    public void righthelper(TreeNode node, List<Integer> ans, int level){
+    public void helper(TreeNode node, List<Integer> ans, int level){
         if(node==null){
             return;
         }
-        if(ans.size()==level){
+        if(level==ans.size()){
             ans.add(node.val);
         }
-        righthelper(node.right,ans,level+1);
-        righthelper(node.left,ans,level+1);
+
+        helper(node.right,ans,level+1);
+        helper(node.left,ans,level+1);
     }
     public List<Integer> rightSideView(TreeNode root) {
-        // recursive - tc: O(n) sc: O(h)
-        List<Integer> ans=new ArrayList<>();
-        righthelper(root,ans,0);
-        return ans;
+        // // recursive - tc: O(n) sc: O(h)
+        // ArrayList<Integer> ans=new ArrayList<>();
+        // helper(root,ans,0);
+        // return ans;
+
+
+        // Iterative - tc: O(n) sc: O(w)
+        ArrayList<Integer> ans=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        
+        if(root==null) return ans;
+        while(!q.isEmpty()){
+            int cursize=q.size();
+
+            for(int i=0;i<cursize;i++){
+                TreeNode node=q.poll();
+                
+                if(i==cursize-1){
+                    ans.add(node.val);
+                }
+
+                if(node.left!=null){
+                    q.add(node.left);
+                }
+                if(node.right!=null){
+                    q.add(node.right);
+                }
+            }
+        }
+
+    return ans;
+
 
     }
 }
