131973078b30a63740c1d59fd0b89cb2da8ab30d
Vishnu Kasilya <125987277+VishnuAmit@users.noreply.github.com>
2024-11-22 09:22:05 +0530

    iter method added

--- BinaryTree/Medium/leftview.java
+++ BinaryTree/Medium/leftview.java
@@ -1,21 +1,51 @@
 // PRoblem link: https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1
-class Tree
-{
-    public void lefthelper(Node node, List<Integer> ans, int level){
-        if(node==null){
+class Solution {
+    // Function to return list containing elements of left view of binary tree.
+    public void helper(Node root, int level, List<Integer> ans){
+        if(root==null){
             return;
         }
-        if(ans.size()==level){
-            ans.add(node.data);
+        
+        if(level==ans.size()){
+            ans.add(root.data);
         }
-        lefthelper(node.left,ans,level+1);
-        lefthelper(node.right,ans,level+1);
+        helper(root.left,level+1,ans);
+        helper(root.right,level+1,ans);
     }
-    ArrayList<Integer> leftView(Node root)
-    {
-           // recursive - tc: O(n) sc: O(h)
+    ArrayList<Integer> leftView(Node root) {
+        // code here
+        //     // tc - O(n) sc: O(h)
+        //   ArrayList<Integer> ans=new ArrayList<>();
+        //   helper(root,0,ans);
+        //   return ans;
+        
+         // Iterative - tc: O(n) sc: O(w)
         ArrayList<Integer> ans=new ArrayList<>();
-        lefthelper(root,ans,0);
-        return ans;
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        
+        if(root==null) return ans;
+        while(!q.isEmpty()){
+            int cursize=q.size();
+
+            for(int i=0;i<cursize;i++){
+                Node node=q.poll();
+                
+                if(i==0){
+                    ans.add(node.data);
+                }
+
+                if(node.left!=null){
+                    q.add(node.left);
+                }
+                if(node.right!=null){
+                    q.add(node.right);
+                }
+            }
+        }
+
+    return ans;
+
+    
     }
 }
