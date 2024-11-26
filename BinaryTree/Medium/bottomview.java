d53219bc6aaf0982283f9524bdcb50ea4af73880
Vishnu Kasilya <125987277+VishnuAmit@users.noreply.github.com>
2024-11-26 17:15:02 +0530

    Update bottomview.java

--- BinaryTree/Medium/bottomview.java
+++ BinaryTree/Medium/bottomview.java
@@ -1,33 +1,43 @@
 // Problem link: https://www.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+class Pair{
+    Node node;
+    int hd;
+    public Pair(Node node, int hd){
+        this.node=node;
+        this.hd=hd;
+    }
+}
+
 class Solution
 {
     //Function to return a list containing the bottom view of the given tree.
     public ArrayList <Integer> bottomView(Node root)
     {
-        // tc: O(n) sc: O(n)
-        Queue<Node> q=new LinkedList<>();
+        //  tc: O(n) sc: O(n)
+        Queue<Pair> q=new LinkedList<>();
         Map<Integer,Integer> mpp=new TreeMap<>();
+        Node node=root;
+        q.add(new Pair(node,0));
         ArrayList<Integer> ans=new ArrayList<>();
-        if(root==null) return ans;
-        root.hd=0;
-        q.add(root);
         while(!q.isEmpty()){
-            Node nd=q.poll();
-            int hd=nd.hd;
+            Pair it=q.poll();
+            Node nd=it.node;
+            int hd=it.hd;
+            
             mpp.put(hd,nd.data);
+            
             if(nd.left!=null){
-                nd.left.hd=hd-1;
-                q.add(nd.left);
+                q.add(new Pair(nd.left,hd-1));
             }
             if(nd.right!=null){
-                nd.right.hd=hd+1;
-                q.add(nd.right);
+                q.add(new Pair(nd.right,hd+1));
             }
         }
-        for(Map.Entry<Integer,Integer> entry:mpp.entrySet()){
-            ans.add(entry.getValue());
         
+        for(Map.Entry<Integer,Integer> itr: mpp.entrySet()){
+            ans.add(itr.getValue());
         }
+        
         return ans;
     }
 }
